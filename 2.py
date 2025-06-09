f8100bf3b54b215884e233d290b9d5e3d77102e0
dndaee <vespa.faber@gmail.com>
2025-06-09 12:13:31 +0300

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,10 +1,11 @@
-def remove_duplicates(input_list):
-    """Функція для видалення повторень зі списку"""
-    return list(set(input_list))
+def find_five_min_elements(lst):
+    """Функція знаходить перші п’ять мінімальних елементів списку"""
+    sorted_list = sorted(lst)
+    return sorted_list[:5]  
 
-user_input = input("Введіть елементи списку через пробіл: ")
-user_list = user_input.split()
+user_input = input("Введіть ціле число через пробіл: ")
+user_list = list(map(int, user_input.split()))
 
-unique_list = remove_duplicates(user_list)
+min_elements = find_five_min_elements(user_list)
 
-print("Список без повторень:", unique_list)
+print("Перші п’ять мінімальних елементів:", min_elements)
